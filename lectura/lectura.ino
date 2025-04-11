8c2c2e3f7720772f97114494202786ea56839e3c
JorgeDS123 <jorgelio4243@gmail.com>
2025-04-10 20:18:18 -0600

    Add files via upload

---
+++ lectura/lectura.ino
@@ -0,0 +1,10 @@
+void setup() {
+    Serial.begin(115200);
+    analogReadResolution(12); // Use 12-bit ADC resolution (0-4095)
+}
+
+void loop() {
+    int analogValue = analogRead(15); // Read ADC pin
+    Serial.println(analogValue); // Send ADC value as a string
+    delay(1); // 1 ms delay = ~1 kHz sampling rate
+}
